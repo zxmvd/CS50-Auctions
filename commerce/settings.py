6282bf1c915eef61a02070070ed203f4dd16bfdd
zxmvd <zx_mailhot@hotmail.com>
2020-11-14 13:11:15 +1100

    static path

--- commerce/settings.py
+++ commerce/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/auctions'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
